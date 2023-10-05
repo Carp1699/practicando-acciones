b758111b8837c2f8eda6db4e267e96d988387bf5
pelayo <crodriguez@griddynamics.com>
2023-10-05 12:35:57 -0600

    New pinshe message

--- src/main/java/com/example/gitactions/Controller.java
+++ src/main/java/com/example/gitactions/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping
     public String hello(){
-        return "Hello world";
+        return "This is not longer hello world";
     }
 }
