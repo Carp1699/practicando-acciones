de89dcd9a95d362204bbf15ee046b240966e21ea
pelayo <crodriguez@griddynamics.com>
2023-10-06 08:26:31 -0600

    Now with build an test commands

--- src/test/java/com/example/gitactions/GitactionsApplicationTests.java
+++ src/test/java/com/example/gitactions/GitactionsApplicationTests.java
@@ -9,5 +9,19 @@ class GitactionsApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	void testeo(){
+		int delayInSeconds = 10; // Set the delay duration in seconds
 
-}
+		try {
+			Thread.sleep(delayInSeconds * 1000); // Convert seconds to milliseconds
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			System.err.println("Thread was interrupted while sleeping");
+		}
+		assert true;
+	}
+
+	}
+
+
